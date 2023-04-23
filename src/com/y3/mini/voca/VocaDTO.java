57689a7f20af0cb12c19aaf47992033f200f89cd
hayeong-yu <seongtan2020@gmail.com>
2023-04-23 15:23:43 +0900

    vocaDTO, vacaManager commit

--- src/com/y3/mini/voca/VocaDTO.java
+++ src/com/y3/mini/voca/VocaDTO.java
@@ -1,16 +1,18 @@
 package com.y3.mini.voca;
 
+import java.util.Arrays;
+
 public class VocaDTO {
 	
 	private String word;
 	private String kormean;
 	private String definition;
-	private String similar;
+	private String[] similar; //유의어를 두개저장
 	
 	public VocaDTO() {}
 	
 	
-	public VocaDTO(String word, String kormean, String definition, String similar) {
+	public VocaDTO(String word, String kormean, String definition, String[] similar) {
 		super();
 		this.word = word;
 		this.kormean = kormean;
@@ -38,24 +40,23 @@ public class VocaDTO {
 	public void setDefinition(String definition) {
 		this.definition = definition;
 	}
-	public String getSimilar() {
+	public String[] getSimilar() {
 		return similar;
 	}
-	public void setSimilar(String similar) {
+	public void setSimilar(String[] similar) {
 		this.similar = similar;
 	}
 
 
 	@Override
 	public String toString() {
-		return "단어 [영단어=" + word + ", 한국어 뜻=" + kormean + ", 영어 정의=" + definition + ", 유의어=" + similar
-				+ "]";
+		return "[영단어=" + word + ", 한국어 뜻=" + kormean + ", 영어 정의=" + definition 
+				+ ", 유의어=" + Arrays.toString(similar) + "]";
 	}
 	
 	
 	
-	
-	
-	
-
 }
+
+
+
--- src/com/y3/mini/voca/VocaManager.java
+++ src/com/y3/mini/voca/VocaManager.java
@@ -1,5 +1,53 @@
 package com.y3.mini.voca;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VocaManager {
+	
+	public List<VocaDTO> vocalist = new ArrayList<>();
+	
+	
+	//단어장 호출
+	public List<VocaDTO> selectAll() { 
+		return vocalist;
+	}
+	
+	//단어검색후 프린트 - 일치하는 단어 없을경우
+	public VocaDTO searchWord(String word) {
+
+	    for (VocaDTO v : vocalist) {
+	        if (v.getWord().equals(word)) {
+	            System.out.println(v.toString());
+	            return v;
+	        }
+	    }
+
+	    System.out.println("입력하신 단어 " + word + " 은/는 단어장에 등록되지 않은 단어입니다.");
+	    return null;
+	}
+	
+	
+	
+	//단어와 단어 관련 정보 추가
+	public void addWord(VocaDTO voca) {
+		vocalist.add(voca);
+	}
+	
+	//단어 삭제
+	public void deleteWord(String word) {
+		
+		for (VocaDTO v : vocalist) {
+	        if (v.getWord().equals(word)) {
+	        	vocalist.remove(v);
+	        	System.out.println("입력하신 단어" + word + "이/가 단어장에서 삭제되었습니다.");
+	            return;
+	        }
+	    }
+	    System.out.println("입력하신 단어 " + word + " 은/는 단어장에 등록되지 않은 단어입니다.");
+		
+	}
+
+
 
 }
