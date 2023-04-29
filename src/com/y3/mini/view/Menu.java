15137d3d652debf5e392f46906e4739759faed65
jeong-yuju <jeong-yuju@localhost>
2023-04-29 18:20:56 +0900

    회원가입 공백 수정

--- src/com/y3/mini/view/Menu.java
+++ src/com/y3/mini/view/Menu.java
@@ -19,10 +19,12 @@ public class Menu {
 	public void mainMenu() {
 
 		while (true) {
-			System.out.println("단어장에 접속했습니다");
+ 			System.out.println("--------------------------------------------------------");
+			System.out.println("단어장에 접속 했습니다");
 			System.out.println("1. 로그인");
 			System.out.println("2. 회원가입");
 			System.out.println("3. 프로그램 종료");
+ 			System.out.println("--------------------------------------------------------");
 			System.out.print("번호 : ");
 			String n = sc.nextLine();
 			if (n.equals("1")) {
@@ -33,26 +35,77 @@ public class Menu {
 				if (isLogin = user.signIn(id, pwd)) {
 					break;
 				}
+				
+//				--------- 기존코드( 회원가입 시 id,pwd,name 입력 받을때 공백 제한하지 않음) ---------
+//			} else if (n.equals("2")) {
+//				System.out.print("아이디 : ");
+//				id = sc.nextLine();
+//				System.out.print("비밀번호 : ");
+//				pwd = sc.nextLine();
+//				System.out.print("이름 : ");
+//				name = sc.nextLine();
+//				
+//				if(!user.signUp(id, pwd, name))
+//					System.out.println("회원가입에 실패했습니다");
+//				
+//			} else if (n.equals("3")) {
+//				System.out.println("프로그램을 종료하겠습니다");
+//				return;
+//			} else {
+//				System.out.println("번호를 잘못입력했습니다");
+//			}
 
+				
+//			--------- 수정코드( 회원가입 시 id,pwd,name 입력 받을때 공백 허용 x ) ---------
 			} else if (n.equals("2")) {
-				System.out.print("아이디 : ");
-				id = sc.nextLine();
-				System.out.print("비밀번호 : ");
-				pwd = sc.nextLine();
-				System.out.print("이름 : ");
-				name = sc.nextLine();
-				
-				if(!user.signUp(id, pwd, name))
-					System.out.println("회원가입에 실패했습니다");
-				
-			} else if (n.equals("3")) {
-				System.out.println("프로그램을 종료하겠습니다");
-				return;
-			} else {
-				System.out.println("번호를 잘못입력했습니다");
-			}
+				String id,pwd,name;
 
+				while(true) {
+				  System.out.print("사용하실 아이디를 입력해 주세요. : ");
+				  id = sc.nextLine().trim(); 			// 공백 제거
+				  if (id.length() == 0) { 			// 아이디가 공백이면(아무것도 입력x)
+				 System.out.println("아이디를 입력해 주세요.");
+				 	continue;
+				  }
+				  if (id.split(" ").length > 1) { 	// 아이디에 공백이 포함되어 있으면
+				 System.out.println("아이디에 공백을 포함할 수 없습니다.");
+				    continue;
+				  }
+				 	break;
+				  }
+				 while (true) {
+				 System.out.print("사용하실 비밀번호를 입력해 주세요. : ");
+				 pwd = sc.nextLine().trim();			 // 공백 제거
+				 if (pwd.length() == 0) { 			 // 비밀번호가 공백이면(아무것도 입력x)
+				 System.out.println("비밀번호를 입력해주세요.");
+				 continue;
+				 }
+				 if (pwd.split(" ").length > 1) { 	// 비밀번호에 공백이 포함되어 있으면
+				 System.out.println("비밀번호에 공백을 포함할 수 없습니다.");
+				 continue;
+				 }
+				 break;
+				 }
+				 while (true) {
+				 System.out.println("이름을 입력해 주세요. : ");
+				 name = sc.nextLine().trim(); 		// 공백 제거
+				 if (name.length() == 0) {		    // 이름이 공백이면(아무것도 입력x)
+				 System.out.println("이름을 입력해주세요.");
+				 continue;
+				 }
+				 if (name.split(" ").length > 1) { 	// 이름에 공백이 포함되어 있으면
+				 System.out.println("이름에 공백을 포함할 수 없습니다.");
+				 continue;
+				 }
+				 break;
+				 }
+
+				if (!user.signUp(id, pwd, name)) {
+				System.out.println("회원가입에 실패했습니다.");
+				 }
 		}
+	}
+
 		while (true) {
 			System.out.println("***단어장 관리 프로그램***");
 			System.out.println("1. 마이페이지");
